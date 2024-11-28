3eb2c927e767ee9c68b68ec1a30b24d673819535
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-11-28 18:27:24 +0400

    add some style to features component

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -10,9 +10,9 @@ export function Features() {
 
   return (
     <section className={classes.features}>
-      <img src="src/assets/guy.png" className={classes.features__img} />
-      <div>
-        <h2>We Provide Many Features You Can Use</h2>
+      <img src="src/assets/guy.png" className={classes.features__guy} />
+      <div className={classes.features__descriptionPart}>
+        <h2 className={classes.features__title}>We Provide Many Features You Can Use</h2>
         <div>
           You can explore the features that we provide with fun and have their
           own functions each feature
