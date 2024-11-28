afbe47460c0f58944f527574978a84e6f2fd8cd5
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-11-28 23:20:23 +0400

    change button`s padding inside design component

--- src/components/Design/Design.tsx
+++ src/components/Design/Design.tsx
@@ -6,7 +6,7 @@ export function Design() {
     <section className={classes.design}>
       <div className={classes.design__choice}>
         <h1>Choose the design you like and apply for card right now</h1>
-        <Button text="Choose the card" padding={8} />
+        <Button text="Choose the card" padding={16} />
       </div>
       <div className={classes.design__cards}>
         <img className={classes.design__card} src="src/assets/card1.png" />
