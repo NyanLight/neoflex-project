007d5f4fdf8f579ec84a8e98524e4e2ec4aee4af
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-12-10 02:58:07 +0400

    adds news components, change margin in map component, two new assets

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { Design } from '../components/Design';
 import { Subscription } from '../components/Subscription';
 import { Features } from '../components/Features';
 import { Currencies } from '../components/Currencies';
+import { News } from '../components/News';
 
 export default function HomePage() {
   return (
@@ -14,6 +15,7 @@ export default function HomePage() {
       <Features />
       <Currencies />
       <Map />
+      <News />
       <Subscription />
       <Footer />
     </>
