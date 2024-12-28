4dd29236f82cfc27d47130f4b705e6ed4552eb9f
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-12-29 00:15:57 +0400

    create input component

---
+++ src/ui/Input/Input.tsx
@@ -0,0 +1,31 @@
+import {FieldValues } from 'react-hook-form';
+import { InputProps } from './types/InputProps.type';
+import { Label } from '../Label';
+import styles from './Input.module.css'
+
+export const Input = <T extends FieldValues>({
+  name,
+  label,
+  register,
+  rules,
+  error,
+  required,
+  ...inputProps
+}: InputProps<T>) => {
+  return (
+    <div className={styles.wrapper}>
+      {label && <Label for={name} required={required} text={label} />}
+      <input
+        id={name}
+        {...register(name, rules)}
+        {...inputProps}
+        className={styles.input}
+      />
+      {error && (
+        <span className={styles.error}>
+          {error.message}
+        </span>
+      )}
+    </div>
+  );
+};
---
+++ src/ui/Input/types/InputProps.type.ts
@@ -0,0 +1,10 @@
+import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+export interface InputProps<T extends FieldValues> {
+    name: Path<T>;
+    label?: string;
+    register: UseFormRegister<T>;
+    rules?: Record<string, unknown>; 
+    error?: FieldError;
+    required: boolean; 
+  }
