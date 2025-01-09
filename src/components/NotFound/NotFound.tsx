9df8fc908e5cbde905af5c5867bd30343df1bf9e
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-09 17:48:46 +0400

    style info part

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../../ui/Button'
 import { Footer } from '../Footer'
 import { Header } from '../Header'
 import styles from './NotFound.module.css'
@@ -8,7 +9,10 @@ export function NotFound () {
         <Header />
         <section className={styles.notFound}>
             <div className={styles.infoPart}>
-
+                <h2 className={styles.infoPart__oops}>Oops....</h2>
+                <h2 className={styles.infoPart__h2}>Page not found</h2>
+                <div className={styles.infoPart__regular}>This Page doesn't exist or was removed! We suggest you go back.</div>
+                <Button borderRadius='8px' horizontalPadding='4rem' verticalPadding='0.75rem' text='Go back' handler={() => {console.log('click')}} />
             </div>
             <div className={styles.imagePart}>
                 <div className={styles.imagePart__wrapper}>
