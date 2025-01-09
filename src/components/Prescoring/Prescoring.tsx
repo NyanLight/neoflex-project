220bd789d21c182e972e8349762e5fdc06b36f8f
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-09 04:53:27 +0400

    replace multiple Inputs with mapping through inputs arrays

--- src/components/Prescoring/Prescoring.tsx
+++ src/components/Prescoring/Prescoring.tsx
@@ -6,17 +6,28 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './Prescoring.module.css';
 import { Divider } from '../../ui/Divider';
 import { useState } from 'react';
-import {  invalidStyle, selectOptions, validStyle } from './constants';
-import { addSpace, validateAge } from './utils';
+import {
+  inputsFirstPart,
+  inputsSecondPart,
+  invalidStyle,
+  selectOptions,
+  validStyle,
+} from './constants';
+import { addSpace, } from './utils';
 import { Loader } from '../../ui/Loader';
 
 export function Prescoring() {
+  const currentStyle = (name: string) => {
+    if (errors[name]) return invalidStyle;
+    if (touchedFields[name] | dirtyFields[name]) return validStyle;
+    return undefined;
+  };
   const [amount, setAmount] = useState<string>('0');
   const {
     register,
     handleSubmit,
     formState: { errors, touchedFields, dirtyFields, isSubmitting },
-  } = useForm<FormFields>({mode: 'onTouched'});
+  } = useForm<FormFields>({ mode: 'onTouched' });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newAmount = addSpace(e.target.value);
@@ -38,7 +49,7 @@ export function Prescoring() {
 
   return (
     <section className={styles.prescoring} id="prescoring">
-      <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+      <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.top}>
           <div className={styles.top__leftSide}>
             <div className={styles.leftSide__infoField}>
@@ -90,59 +101,21 @@ export function Prescoring() {
         <div className={styles.contact}>
           <h3 className={styles.contact__title}>Contact information</h3>
           <div className={styles.contact__inputs}>
-            <div className={styles.input}>
-              <Input
-                type="text"
-                required={true}
-                style={
-                  touchedFields.lastName || dirtyFields.lastName
-                    ? !errors.lastName
-                      ? validStyle
-                      : invalidStyle
-                    : undefined
-                }
-                register={register}
-                error={errors.lastName}
-                name="lastName"
-                label="Your last name"
-                placeholder="For Example Doe"
-                rules={{ required: 'Enter your last name' }}
-              />
-            </div>
-            <div className={styles.input}>
-              <Input
-                type="text"
-                required={true}
-                register={register}
-                style={
-                  touchedFields.firstName || dirtyFields.firstName
-                    ? !errors.firstName
-                      ? validStyle
-                      : invalidStyle
-                    : undefined
-                }
-                error={errors.firstName}
-                name="firstName"
-                label="Your first name"
-                placeholder="For Example John"
-                rules={{ required: 'Enter your first name' }}
-              />
-            </div>
-            <Input
-              type="text"
-              required={false}
-              register={register}
-              name="middleName"
-              style={
-                touchedFields.middleName || dirtyFields.middleName
-                  ? !errors.middleName
-                    ? validStyle
-                    : invalidStyle
-                  : undefined
-              }
-              label="Your patronymic"
-              placeholder="For Example Victorovich"
-            />
+            {inputsFirstPart.map((input, index) => (
+              <div className={styles.input} key={index}>
+                <Input
+                  name={input.name}
+                  register={register}
+                  type={input.type}
+                  required={input.required}
+                  label={input.label}
+                  placeholder={input.placeholder}
+                  rules={input.rules}
+                  error={errors[input.name]}
+                  style={currentStyle(input.name)}
+                />
+              </div>
+            ))}
             <Select
               required={true}
               register={register}
@@ -151,121 +124,38 @@ export function Prescoring() {
               options={selectOptions}
               rules={{ required: true }}
             />
-            <div className={styles.input}>
-              <Input
-                type="email"
-                required={true}
-                register={register}
-                style={
-                  touchedFields.email || dirtyFields.email
-                    ? !errors.email
-                      ? validStyle
-                      : invalidStyle
-                    : undefined
-                }
-                name="email"
-                label="Your email"
-                error={errors.email}
-                placeholder="test@gmail.com"
-                rules={{
-                  required: 'Field is required',
-                  pattern: {
-                    value:
-                      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'Incorrect email address',
-                  },
-                }}
-              />
-            </div>
-            <div className={styles.input}>
-              <Input
-                type="date"
-                required={true}
-                register={register}
-                style={
-                  touchedFields.birthdate || dirtyFields.birthdate
-                    ? !errors.birthdate
-                      ? undefined
-                      : { border: '2px solid #FF5631' }
-                    : undefined
-                }
-                name="birthdate"
-                label="Your date of birth"
-                error={errors.birthdate}
-                placeholder="Select Date and Time"
-                rules={{
-                  required: 'Enter your date of birth',
-                  validate: (value: Date) => validateAge(value),
-                }}
-              />
-            </div>
-            <div className={styles.input}>
-              <Input
-                type="number"
-                required={true}
-                register={register}
-                style={
-                  touchedFields.passportSeries || dirtyFields.passportSeries
-                    ? !errors.passportSeries
-                      ? validStyle
-                      : invalidStyle
-                    : undefined
-                }
-                error={errors.passportSeries}
-                name="passportSeries"
-                label="Your passport series"
-                placeholder="0000"
-                rules={{
-                  required: 'Enter your passport number',
-                  pattern: {
-                    value: /^[0-9]{4}$/,
-                    message: 'The series must be 4 digits',
-                  },
-                }}
-              />
-            </div>
-            <div className={styles.input}>
-              <Input
-                type="number"
-                required={true}
-                register={register}
-                style={
-                  touchedFields.passportNumber || dirtyFields.passportNumber
-                    ? !errors.passportNumber
-                      ? validStyle
-                      : invalidStyle
-                    : undefined
-                }
-                error={errors.passportNumber}
-                name="passportNumber"
-                label="Your passport number"
-                placeholder="000000"
-                rules={{
-                  required: 'Enter your passport number',
-                  pattern: {
-                    value: /^[0-9]{6}$/,
-                    message: 'The series must be 6 digits',
-                  },
-                }}
-              />
-            </div>
+             {inputsSecondPart.map((input, index) => (
+              <div className={styles.input} key={index}>
+                <Input
+                  name={input.name}
+                  register={register}
+                  type={input.type}
+                  required={input.required}
+                  label={input.label}
+                  placeholder={input.placeholder}
+                  rules={input.rules}
+                  error={errors[input.name]}
+                  style={currentStyle(input.name)}
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className={styles.submit}>
           {!isSubmitting ? (
             <>
-            <Loader isDisplaying={false} />  
-            <Button
-              text="Continue"
-              borderRadius="8px"
-              verticalPadding="1rem"
-              horizontalPadding="2rem"
-              handler={() => {
-                return;
-              }}
-            />
-            </>)
-             : (
+              <Loader isDisplaying={false} />
+              <Button
+                text="Continue"
+                borderRadius="8px"
+                verticalPadding="1rem"
+                horizontalPadding="2rem"
+                handler={() => {
+                  return;
+                }}
+              />
+            </>
+          ) : (
             <Loader isDisplaying={true} />
           )}
         </div>
