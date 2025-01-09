69df42516bd1df1833efbceff00a4362523c3f16
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-09 18:02:32 +0400

    add button handler to go to the previous page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -2,8 +2,15 @@ import { Button } from '../../ui/Button'
 import { Footer } from '../Footer'
 import { Header } from '../Header'
 import styles from './NotFound.module.css'
+import { useNavigate } from 'react-router'
 
 export function NotFound () {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);        
+    }
+
     return (
         <>
         <Header />
@@ -12,7 +19,7 @@ export function NotFound () {
                 <h2 className={styles.infoPart__oops}>Oops....</h2>
                 <h2 className={styles.infoPart__h2}>Page not found</h2>
                 <div className={styles.infoPart__regular}>This Page doesn't exist or was removed! We suggest you go back.</div>
-                <Button borderRadius='8px' horizontalPadding='4rem' verticalPadding='0.75rem' text='Go back' handler={() => {console.log('click')}} />
+                <Button borderRadius='8px' horizontalPadding='4rem' verticalPadding='0.75rem' text='Go back' handler={goBack} />
             </div>
             <div className={styles.imagePart}>
                 <div className={styles.imagePart__wrapper}>
