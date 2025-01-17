b9303ca6ae139a832c0b48f55e7b9327d9c67846
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-18 03:51:27 +0400

    create table component

--- src/components/Document/Document.tsx
+++ src/components/Document/Document.tsx
@@ -1,16 +1,84 @@
-import styles from './Document.module.css'
+import { Table } from '../../ui/Table';
+import styles from './Document.module.css';
 
-export function Document () {
-    return (
-         <section className={styles.document}>
-            <div className={styles.wrapper}>
-                <div className={styles.info}>
-                <h2 className={styles.document__title}>
-                Payment Schedule
-          </h2>
+export function Document() {
+  const headers = [
+    { label: 'NUMBER', key: 'number' },
+    { label: 'DATE', key: 'date' },
+    { label: 'TOTAL PAYMENT', key: 'totalPayment' },
+    { label: 'INTEREST PAYMENT', key: 'interestPayment' },
+    { label: 'DEBT PAYMENT', key: 'debtPayment' },
+    { label: 'REMAINING DEBT', key: 'remainingDebt' },
+  ];
+
+  const data = [
+    {
+      number: 0,
+      date: '2025-01-17',
+      totalPayment: 0,
+      interestPayment: 0,
+      debtPayment: 0,
+      remainingDebt: 108000.0,
+    },
+    {
+      number: 1,
+      date: '2025-02-17',
+      totalPayment: 18420.04,
+      interestPayment: 720.01,
+      debtPayment: 17700.03,
+      remainingDebt: 90299.97,
+    },
+    {
+      number: 2,
+      date: '2025-03-17',
+      totalPayment: 18420.04,
+      interestPayment: 602.0,
+      debtPayment: 17818.04,
+      remainingDebt: 72481.93,
+    },
+    {
+      number: 3,
+      date: '2025-04-17',
+      totalPayment: 18420.04,
+      interestPayment: 483.22,
+      debtPayment: 17936.82,
+      remainingDebt: 54545.11,
+    },
+    {
+      number: 4,
+      date: '2025-05-17',
+      totalPayment: 18420.04,
+      interestPayment: 363.64,
+      debtPayment: 18056.4,
+      remainingDebt: 36488.71,
+    },
+    {
+      number: 5,
+      date: '2025-06-17',
+      totalPayment: 18420.04,
+      interestPayment: 243.26,
+      debtPayment: 18176.78,
+      remainingDebt: 18311.93,
+    },
+    {
+      number: 6,
+      date: '2025-07-17',
+      totalPayment: 18420.04,
+      interestPayment: 122.08,
+      debtPayment: 18297.96,
+      remainingDebt: 13.97,
+    },
+  ];
+
+  return (
+    <section className={styles.document}>
+      <div className={styles.wrapper}>
+        <div className={styles.info}>
+          <h2 className={styles.document__title}>Payment Schedule</h2>
           <div className={styles.document__step}>Step 3 of 5</div>
-                </div>
-            </div>
-         </section>
-    )
+        </div>
+        <Table data={data} headers={headers} />
+      </div>
+    </section>
+  );
 }
---
+++ src/ui/Table/Table.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styles from './Table.module.css';
+
+export function Table({
+  data,
+  headers,
+}: {
+  headers: Record<string, string>[];
+  data: {
+    number: number;
+    date: string;
+    totalPayment: number;
+    interestPayment: number;
+    debtPayment: number;
+    remainingDebt: number;
+  }[];
+}) {
+  const [sort, setSort] = useState({keyToSort: 'number', direction: 'asc'})
+
+  function handleHeaderClick (header) {
+    setSort({
+      keyToSort: header.key,
+      direction: header.key === sort.keyToSort ? sort.direction === 'asc' ? 'desc' : 'asc' : 'asc'  
+    })
+  }
+
+  function getSortedArray(array) {
+    if (sort.direction === 'asc') {
+      return array.sort((a, b) => (a[sort.keyToSort] > b[sort.keyToSort] ? 1 : -1))
+    }
+    return array.sort((a, b) => (a[sort.keyToSort] > b[sort.keyToSort] ? -1 : 1))
+  }
+
+  return (
+    <table className={styles.table}>
+      <thead className={styles.table__headers}>
+        <tr>
+          {headers.map((header) => (
+            <th onClick={() => handleHeaderClick(header)} key={header.key} className={styles.table__header}>
+              <span>{header.label}</span>
+              <span><img className={sort.keyToSort === header.key && sort.direction === 'asc' ? styles.table__desc : styles.table__icon } src="/src/assets/caret.png" alt="" /></span>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {getSortedArray(data).map((row, index: number) => (
+          <tr key={index} className={styles.table__regularRow}>
+            <td className={styles.table__regularCell}>{row.number}</td>
+            <td className={styles.table__regularCell}>{row.date}</td>
+            <td className={styles.table__regularCell}>{row.totalPayment}</td>
+            <td className={styles.table__regularCell}>{row.interestPayment}</td>
+            <td className={styles.table__regularCell}>{row.debtPayment}</td>
+            <td className={styles.table__regularCell}>{row.remainingDebt}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
