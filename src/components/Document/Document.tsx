f446c8b78e909581acdd78cc2d5fbe76451baaa4
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-18 04:47:26 +0400

    add styling to modal

--- src/components/Document/Document.tsx
+++ src/components/Document/Document.tsx
@@ -1,75 +1,15 @@
+import { useState } from 'react';
 import { Button } from '../../ui/Button';
 import { Table } from '../../ui/Table';
 import styles from './Document.module.css';
+import { data, headers } from './constants';
 
 export function Document() {
-  const headers = [
-    { label: 'NUMBER', key: 'number' },
-    { label: 'DATE', key: 'date' },
-    { label: 'TOTAL PAYMENT', key: 'totalPayment' },
-    { label: 'INTEREST PAYMENT', key: 'interestPayment' },
-    { label: 'DEBT PAYMENT', key: 'debtPayment' },
-    { label: 'REMAINING DEBT', key: 'remainingDebt' },
-  ];
+  const [modal, setModal] = useState(false);
 
-  const data = [
-    {
-      number: 0,
-      date: '2025-01-17',
-      totalPayment: 0,
-      interestPayment: 0,
-      debtPayment: 0,
-      remainingDebt: 108000.0,
-    },
-    {
-      number: 1,
-      date: '2025-02-17',
-      totalPayment: 18420.04,
-      interestPayment: 720.01,
-      debtPayment: 17700.03,
-      remainingDebt: 90299.97,
-    },
-    {
-      number: 2,
-      date: '2025-03-17',
-      totalPayment: 18420.04,
-      interestPayment: 602.0,
-      debtPayment: 17818.04,
-      remainingDebt: 72481.93,
-    },
-    {
-      number: 3,
-      date: '2025-04-17',
-      totalPayment: 18420.04,
-      interestPayment: 483.22,
-      debtPayment: 17936.82,
-      remainingDebt: 54545.11,
-    },
-    {
-      number: 4,
-      date: '2025-05-17',
-      totalPayment: 18420.04,
-      interestPayment: 363.64,
-      debtPayment: 18056.4,
-      remainingDebt: 36488.71,
-    },
-    {
-      number: 5,
-      date: '2025-06-17',
-      totalPayment: 18420.04,
-      interestPayment: 243.26,
-      debtPayment: 18176.78,
-      remainingDebt: 18311.93,
-    },
-    {
-      number: 6,
-      date: '2025-07-17',
-      totalPayment: 18420.04,
-      interestPayment: 122.08,
-      debtPayment: 18297.96,
-      remainingDebt: 13.97,
-    },
-  ];
+  const toggleModal = () => {
+    setModal(!modal);
+  };
 
   return (
     <section className={styles.document}>
@@ -83,11 +23,47 @@ export function Document() {
         </div>
         <div className={styles.buttons}>
           <div className={styles.denyPart}>
-          <Button handler={() => console.log('deny')} horizontalPadding='1.75rem' verticalPadding='0.75rem'  text='Deny'  borderRadius='8px' />
+            <Button
+              handler={toggleModal}
+              horizontalPadding="1.75rem"
+              verticalPadding="0.75rem"
+              text="Deny"
+              borderRadius="8px"
+            />
           </div>
           <div className={styles.acceptPart}></div>
         </div>
       </div>
+      {modal && (
+        <div className={styles.modal}>
+          <div className={styles.modal__overlay} onClick={toggleModal}></div>
+          <div className={styles.modal__content}>
+            <div className={styles.modal__title}>Deny application</div>
+            <div className={styles.modal__regular}>
+              You exactly sure, you want to cancel this application?
+            </div>
+            <div className={styles.modal__buttons}>
+              <Button
+                borderRadius="8px"
+                text="Deny"
+                horizontalPadding="1.75rem"
+                verticalPadding="0.75rem"
+                handler={() => console.log('deny in modal')}
+              />
+              <Button
+                borderRadius="8px"
+                text="Cancel"
+                horizontalPadding="1.75rem"
+                verticalPadding="0.75rem"
+                handler={() => console.log('cancel in modal')}
+              />
+            </div>
+            <div className={styles.modal__close} onClick={toggleModal}>
+              <img src="/src/assets/modal_cross.png" alt="" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
