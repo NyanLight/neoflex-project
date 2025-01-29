29d3f74e5dd4b10d395478ce838871e7e00985dc
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-29 09:14:45 +0400

    add tests for prescoring component

---
+++ src/components/Prescoring/__tests__/Prescoring.test.tsx
@@ -0,0 +1,20 @@
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { describe, it, expect, afterEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { Prescoring } from '../Prescoring';
+
+describe('Prescoring Component', () => {
+    afterEach(cleanup);
+
+    it('renders the form correctly', () => {
+      render(<Prescoring />);
+      expect(screen.getByText(/customize your card/i)).toBeInTheDocument();
+    });
+    it('updates the amount input correctly', () => {
+      render(<Prescoring />);
+      const input = screen.getByRole('slider');
+      fireEvent.change(input, { target: { value: '30000' } });
+      expect(screen.getAllByText(/30 000/i)[0]).toBeInTheDocument();
+    });
+  });
+  
